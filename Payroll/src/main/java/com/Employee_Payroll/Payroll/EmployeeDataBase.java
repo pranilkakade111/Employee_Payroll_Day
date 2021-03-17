a2bcd3f0bf9a87f4ac1e3a76c9510457c78b8b3c
Pranil Kakade <pranilkakade111@gmail.com>
2021-03-17 20:00:13 +0530

    To establish database for employee

--- Payroll/src/main/java/com/Employee_Payroll/Payroll/EmployeeDataBase.java
+++ Payroll/src/main/java/com/Employee_Payroll/Payroll/EmployeeDataBase.java
@@ -1,5 +1,113 @@
 package com.Employee_Payroll.Payroll;
 
-public class EmployeeDataBase {
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeeDataBase 
+{
+	static Scanner sc= new Scanner(System.in);
+	static boolean exit=false;
+	static Connection connection;
+	static Statement statement;
+	static String jdbcUrl="jdbc:mysql://localhost:3306/employee?useSSL=false";
+	static String username="root";
+	static String password="root";
+	public static Connection getConnection() 
+	{
+		try 
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded");
+			System.out.println("Connecting to database "+jdbcUrl);
+			connection=DriverManager.getConnection(jdbcUrl,username,password);
+			System.out.println("done!!!");
+
+
+		}
+		catch(ClassNotFoundException | SQLException  e)
+		{
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
+	public static List<Employee> display_EmployeeDetails() throws SQLException
+	{ 
+
+
+		List<Employee> Employe = new ArrayList<>();
+		try 
+		{
+			connection=getConnection();
+			statement= connection.createStatement();
+			ResultSet result = statement.executeQuery("select * from employee_system");
+			while(result.next())
+			{
+				Employee e= new Employee();
+				e.setId(result.getInt(1));
+				e.setName(result.getString(2));
+				e.setGender(result.getString(3));
+				e.setSalary(result.getDouble(4));
+				e.setAdress(result.getString(5));
+				e.setPhone_no(result.getString(6));
+				e.setDate_join(LocalDate.parse(result.getString(7)));
+				Employe.add(e);
+
+			}
+			connection.close();
+
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+
+		return Employe;
+	}
+
+	public static List<Salary> getSalaryDetails() throws SQLException
+	{
+		List<Salary> salarylist =new ArrayList<>();
+
+		try {
+			connection=getConnection();
+			statement= connection.createStatement();
+			ResultSet result = statement.executeQuery("SELECT * from earning");
+			while(result.next())
+			{
+				Salary sal = new Salary();
+				sal.setId(result.getInt(1));
+				sal.setBasic_Pay(result.getDouble(2));
+				sal.setDeduction(result.getDouble(3));
+				sal.setTaxPay(result.getDouble(4));
+				sal.setTax(result.getDouble(5));
+				sal.setE_id(result.getInt(6));
+				salarylist.add(sal);
+			}
+			connection.close();
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+
+		return salarylist;
+
+	}
+
+	public static void main(String[] args)
+	{
+
+	}
 
 }
--- Payroll/src/main/java/com/Employee_Payroll/Payroll/Salary.java
+++ Payroll/src/main/java/com/Employee_Payroll/Payroll/Salary.java
@@ -1,5 +1,91 @@
 package com.Employee_Payroll.Payroll;
 
-public class Salary {
+public class Salary 
+{
+	  private int id;
+	  private double basic_Pay;
+	  private double deduction;
+	  private double taxPay;
+	  private double tax;
+	  private int E_id;
+	  
+	  
+	public int getId() 
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public double getBasic_Pay() 
+	{
+		return basic_Pay;
+	}
+	
+	
+	public void setBasic_Pay(double basic_Pay) 
+	{
+		this.basic_Pay = basic_Pay;
+	}
+	
+	
+	public double getDeduction() 
+	{
+		return deduction;
+	}
+	
+	
+	public void setDeduction(double deduction) 
+	{
+		
+		this.deduction = deduction;
+	}
+	
+	
+	public double getTaxPay() 
+	{
+		return taxPay;
+	}
+	
+	
+	public void setTaxPay(double taxPay)
+	{
+		this.taxPay = taxPay;
+	}
+	
+	
+	public double getTax() 
+	{
+		return tax;
+	}
+	
+	
+	public void setTax(double tax) 
+	{
+		this.tax = tax;
+	}
+	
+	
+	public int getE_id() 
+	{
+		return E_id;
+	}
+	
+	
+	public void setE_id(int e_id) 
+	{
+		E_id = e_id;
+	}
 
+	@Override
+	public String toString() {
+		return "Salary [id=" + id + 
+				", basic_Pay=" + basic_Pay + 
+				", deduction=" + deduction + 
+				", taxPay=" + taxPay+
+				", tax=" + tax + ", E_id=" + E_id + "]";
+}
 }
